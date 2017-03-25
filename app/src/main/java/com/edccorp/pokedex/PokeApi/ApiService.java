fb97081a33b3bfbce60832f807f4c6175a245acc
FameEdc <1507Cha9624>
2017-03-25 16:34:27 +0100

    ApiService

--- app/src/main/java/com/edccorp/pokedex/PokeApi/ApiService.java
+++ app/src/main/java/com/edccorp/pokedex/PokeApi/ApiService.java
@@ -1,8 +1,17 @@
 package com.edccorp.pokedex.PokeApi;
 
+import retrofit2.Call;
+import com.edccorp.pokedex.Models.PokemonResponse;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by ribei on 25/03/2017.
  */
 
 public interface ApiService {
+
+    @GET("pokemon")
+    Call<PokemonResponse> getListPokemon(@Query("limit") int limit, @Query("offset") int offset);
 }
